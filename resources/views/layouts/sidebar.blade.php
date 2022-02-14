564bf370de826a5f4d955101a1ae4573361ddbc6
c-syamsulhadi-si <c.syamsulhadi.si@gmail.com>
2022-02-15 04:00:58 +0800

    class 'active' menu sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -5,43 +5,56 @@
         </div>
         <div class="sidebar-brand-text mx-3">E-SPK-SMART</div>
     </a>
-    <hr class="sidebar-divider">
-    <div class="sidebar-heading">
+    {{-- <div class="sidebar-heading">
         Fitur
-    </div>
+    </div> --}}
         <li class="nav-item">
             <a class="nav-link" href="/">
                 <i class="fas fa-home"></i>
                 <span>Beranda</span>
             </a>
         </li>
-        <li class="nav-item">
-            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTable1" aria-expanded="false" aria-controls="collapseTable1">
-            <i class="fas fa-th-large"></i>
-            <span>Kriteria</span>
+        <hr class="sidebar-divider">
+        <div class="sidebar-heading">
+            Kriteria
+        </div>
+        <li class="nav-item {{ Request::is('kriteria*') ? 'active' : '' }}">
+            <a class="nav-link" href="{{ route('kriteria.index') }}">
+                <i class="fas fa-th-large"></i>
+                <span>Kriteria</span>
             </a>
-            <div id="collapseTable1" class="collapse" aria-labelledby="headingTable" data-parent="#accordionSidebar" style="">
-            <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Tambah Data</h6>
-                <a class="collapse-item" href="{{ route('kriteria.index') }}">Kriteria</a>
-                <a class="collapse-item" href="{{ route('parameter.index') }}">Parameter Kriteria</a>
-            </div>
-            </div>
         </li>
-        <li class="nav-item">
-            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTable2" aria-expanded="false" aria-controls="collapseTable2">
-            <i class="fas fa-rocket"></i>
-            <span>Alternatif</span>
+        <li class="nav-item {{ Request::is('parameter*') ? 'active' : '' }}">
+            <a class="nav-link" href="{{ route('parameter.index') }}">
+                <i class="fas fa-cubes"></i>
+                <span>Parameter Kriteria</span>
             </a>
-            <div id="collapseTable2" class="collapse" aria-labelledby="headingTable" data-parent="#accordionSidebar" style="">
-            <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Tambah Data</h6>
-                <a class="collapse-item" href="{{ route('alternatif.index') }}">Alternatif</a>
-                <a class="collapse-item" href="{{ route('nilai.index') }}">Nilai Alternatif</a>
-            </div>
-            </div>
         </li>
-        <li class="nav-item">
+        <hr class="sidebar-divider">
+        <div class="sidebar-heading">
+            Alternatif
+        </div>
+        <li class="nav-item {{ Request::is('alternatif*') ? 'active' : '' }}">
+            <a class="nav-link" href="{{ route('alternatif.index') }}">
+                <i class="fas fa-rocket"></i>
+                <span>Alternatif</span>
+            </a>
+        </li>
+        <hr class="sidebar-divider">
+        <div class="sidebar-heading">
+            Input
+        </div>
+        <li class="nav-item {{ Request::is('nilai*') ? 'active' : '' }}">
+            <a class="nav-link" href="{{ route('nilai.index') }}">
+                <i class="fas fa-pencil-alt"></i>
+                <span>Nilai Alternatif</span>
+            </a>
+        </li>
+        <hr class="sidebar-divider">
+        <div class="sidebar-heading">
+            fitur
+        </div>
+        <li class="nav-item {{ Route::currentRouteName('perhitungan.kalkulasi') ? 'active' : '' }}">
             <a class="nav-link" href="{{ route('perhitungan.kalkulasi') }}">
                 <i class="fas fa-puzzle-piece"></i>
                 <span>Perhitungan</span>
@@ -50,7 +63,7 @@
     <li class="nav-item">
             <a class="nav-link" href="#">
                 <i class="fas fa-sign-out-alt"></i>
-                <span>Logout</span>
+                <span>Keluar</span>
                 <form id="logout-form" action="" method="POST"style="display: none;">
                     @csrf
                 </form>
